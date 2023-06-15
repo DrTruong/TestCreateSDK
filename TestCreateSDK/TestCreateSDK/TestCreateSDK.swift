7a0fc74d1f01e4c0a88d7f7365d087bcc9e5fb4d
Spacecy Truong <space.truong@gmail.com>
2023-06-15 09:49:41 +0700

    create project - first build

---
+++ TestCreateSDK/TestCreateSDK/TestCreateSDK.swift
@@ -0,0 +1,14 @@
+//
+//  TestCreateSDK.swift
+//  TestCreateSDK
+//
+//  Created by Truong on 15/06/2023.
+//
+
+import Foundation
+
+public class TestCreateSDK {
+    public static func add(_ a: Int, with b: Int) -> Int {
+        return a + b
+    }
+}
